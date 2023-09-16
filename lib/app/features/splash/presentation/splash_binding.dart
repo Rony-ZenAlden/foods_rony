e7b5e539379831591ca6b99ca5d9489183816333
ROG <steam12345rony@gmail.com>
2023-09-16 17:23:21 +0300

    Do some Changes

--- lib/app/features/splash/presentation/splash_binding.dart
+++ lib/app/features/splash/presentation/splash_binding.dart
@@ -5,8 +5,8 @@ import 'splash_controller.dart';
 class SplashBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(
-      () => SplashController(),
+    Get.put(
+      SplashController(),
     );
   }
 }
